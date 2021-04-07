e52cba664916d0fa895152bf6bc56379dabb7fed
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2021-04-07 15:38:41 -0400

    added gitignore and steering

--- aim_line_follow/aim_line_follow.py
+++ aim_line_follow/aim_line_follow.py
@@ -11,11 +11,14 @@ from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float64
 from nxp_cup_interfaces.msg import PixyVector
 from time import sleep
+from datetime import datetime
+import numpy as np
 
 class LineFollow(Node):
 
     def __init__(self):
         super().__init__('aim_line_follow')
+
 
         start_delay_descriptor = ParameterDescriptor(
             type=ParameterType.PARAMETER_DOUBLE,
@@ -32,6 +35,10 @@ class LineFollow(Node):
         angular_velocity_descriptor = ParameterDescriptor(
             type=ParameterType.PARAMETER_DOUBLE,
             description='Angular velocity for vehicle motion (rad/s).')
+
+        single_line_steer_scale_descriptor = ParameterDescriptor(
+            type=ParameterType.PARAMETER_DOUBLE,
+            description='Single found line steer scaling.')
         
         self.declare_parameter("start_delay", 15.0, 
             start_delay_descriptor)
@@ -45,6 +52,9 @@ class LineFollow(Node):
         self.declare_parameter("angular_velocity", 1.5, 
             angular_velocity_descriptor)
 
+        self.declare_parameter("single_line_steer_scale", 0.5, 
+            single_line_steer_scale_descriptor)
+
         self.start_delay = float(self.get_parameter("start_delay").value)
 
         self.camera_vector_topic = str(self.get_parameter("camera_vector_topic").value)
@@ -53,10 +63,15 @@ class LineFollow(Node):
 
         self.angular_velocity = float(self.get_parameter("angular_velocity").value)
 
+        self.single_line_steer_scale = float(self.get_parameter("single_line_steer_scale").value)
+
         # Time to wait before running
         self.get_logger().info('Waiting to start for {:s}'.format(str(self.start_delay)))
         sleep(self.start_delay)
         self.get_logger().info('Started')
+
+        self.start_time = datetime.now().timestamp()
+        self.restart_time = True
 
         # Subscribers
         self.pixy_subscriber = self.create_subscription(
@@ -90,6 +105,7 @@ class LineFollow(Node):
 
     def listener_callback(self, msg):
         #TODO
+        current_time = datetime.now().timestamp()
         frame_width = 79
         frame_height = 52
         window_center = (frame_width / 2)
@@ -100,10 +116,19 @@ class LineFollow(Node):
         num_vectors = self.get_num_vectors(msg)
 
         if(num_vectors == 0):
+            if self.restart_time:
+                self.start_time = datetime.now().timestamp()
+                self.restart_time = False
+            if (self.start_time+4.0) > current_time:
+                speed = self.linear_velocity * (4.0-(current_time-self.start_time))/4.0
+            if (self.start_time+4.0) <= current_time:
+                speed = 0
             steer = 0
-            speed = 0
 
         if(num_vectors == 1):
+            if not self.restart_time:
+                self.start_time = datetime.now().timestamp()
+                self.restart_time = True
             if(msg.m0_x1 > msg.m0_x0):
                 x = (msg.m0_x1 - msg.m0_x0) / frame_width
                 y = (msg.m0_y1 - msg.m0_y0) / frame_height
@@ -111,18 +136,30 @@ class LineFollow(Node):
                 x = (msg.m0_x0 - msg.m0_x1) / frame_width
                 y = (msg.m0_y0 - msg.m0_y1) / frame_height
             if(msg.m0_x0 != msg.m0_x1 and y != 0):
-                steer = (-self.angular_velocity) * (x / y)
-                speed = self.linear_velocity
+                steer = (-self.angular_velocity) * (x / y) * self.single_line_steer_scale
+                if (self.start_time+4.0) > current_time:
+                    speed = self.linear_velocity * ((current_time-self.start_time)/4.0)
+                if (self.start_time+4.0) <= current_time:
+                    speed = self.linear_velocity
             else:
                 steer = 0
-                speed = self.linear_velocity*0.9
+                if (self.start_time+4.0) > current_time:
+                    speed = self.linear_velocity * ((current_time-self.start_time)/4.0)*0.9
+                if (self.start_time+4.0) <= current_time:
+                    speed = self.linear_velocity*0.9
 
         if(num_vectors == 2):
+            if not self.restart_time:
+                self.start_time = datetime.now().timestamp()
+                self.restart_time = True
             m_x1 = (msg.m0_x1 + msg.m1_x1) / 2
             steer = self.angular_velocity*(m_x1 - window_center) / frame_width
-            speed = self.linear_velocity
+            if (self.start_time+4.0) > current_time:
+                speed = self.linear_velocity * ((current_time-self.start_time)/4.0)
+            if (self.start_time+4.0) <= current_time:
+                speed = self.linear_velocity
 
-        self.speed_vector.x = float(speed)
+        self.speed_vector.x = float(speed*(1-np.abs(2.0*steer)))
         self.steer_vector.z = float(steer)
 
         self.cmd_vel.linear = self.speed_vector
