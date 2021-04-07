e7bf447fa87ea0ba0b4bb16ba38b74e269cd73f6
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2021-04-07 12:56:30 -0400

    added params

--- aim_line_follow/aim_line_follow.py
+++ aim_line_follow/aim_line_follow.py
@@ -1,97 +1,144 @@
 import rclpy
 from rclpy.node import Node
 
+from rclpy.exceptions import ParameterNotDeclaredException
+from rcl_interfaces.msg import Parameter
+from rcl_interfaces.msg import ParameterType
+from rcl_interfaces.msg import ParameterDescriptor
+
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 from std_msgs.msg import Float64
 from nxp_cup_interfaces.msg import PixyVector
+from time import sleep
 
 class LineFollow(Node):
 
-	def __init__(self):
-		super().__init__('aim_line_follow')
+    def __init__(self):
+        super().__init__('aim_line_follow')
 
-		# Subscribers
-		self.pixy_subscriber = self.create_subscription(
-			PixyVector,
-			'/cupcar0/PixyVector',
-			self.listener_callback,
-			10)
+        start_delay_descriptor = ParameterDescriptor(
+            type=ParameterType.PARAMETER_DOUBLE,
+            description='Seconds to delay before starting.')
 
-		# Publishers
-		self.cmd_vel_publisher = self.create_publisher(Twist, '/cupcar0/cmd_vel', 10)
+        camera_vector_topic_descriptor = ParameterDescriptor(
+            type=ParameterType.PARAMETER_STRING,
+            description='Namespaceing with camera topic.')
+        
+        linear_velocity_descriptor = ParameterDescriptor(
+            type=ParameterType.PARAMETER_DOUBLE,
+            description='Linear velocity for vehicle motion (m/s).')
 
-		self.speed_vector = Vector3()
-		self.steer_vector = Vector3()
-		self.cmd_vel = Twist()
+        angular_velocity_descriptor = ParameterDescriptor(
+            type=ParameterType.PARAMETER_DOUBLE,
+            description='Angular velocity for vehicle motion (rad/s).')
+        
+        self.declare_parameter("start_delay", 15.0, 
+            start_delay_descriptor)
+        
+        self.declare_parameter("camera_vector_topic", "/cupcar0/PixyVector", 
+            camera_vector_topic_descriptor)
+        
+        self.declare_parameter("linear_velocity", 1.25, 
+            linear_velocity_descriptor)
 
-		# Timer setup
-		# timer_period = 0.5 #seconds
-		# self.timer = self.create_timer(timer_period, self.timer_callback)
-		# self.i = 0
+        self.declare_parameter("angular_velocity", 1.5, 
+            angular_velocity_descriptor)
 
-	def get_num_vectors(self, msg):
-		num_vectors = 0
-		if(not(msg.m0_x0 == 0 and msg.m0_x1 == 0 and msg.m0_y0 == 0 and msg.m0_y1 == 0)):
-			num_vectors = num_vectors + 1
-		if(not(msg.m1_x0 == 0 and msg.m1_x1 == 0 and msg.m1_y0 == 0 and msg.m1_y1 == 0)):
-			num_vectors = num_vectors + 1
-		return num_vectors
+        self.start_delay = float(self.get_parameter("start_delay").value)
 
-	# def timer_callback(self):
-	# 	#TODO
+        self.camera_vector_topic = str(self.get_parameter("camera_vector_topic").value)
 
-	def listener_callback(self, msg):
-		#TODO
-		frame_width = 79
-		frame_height = 52
-		window_center = (frame_width / 2)
-		x = 0
-		y = 0
-		steer = 0
-		speed = 0
-		num_vectors = self.get_num_vectors(msg)
+        self.linear_velocity = float(self.get_parameter("linear_velocity").value)
 
-		if(num_vectors == 0):
-			steer = 0
-			speed = 0
+        self.angular_velocity = float(self.get_parameter("angular_velocity").value)
 
-		if(num_vectors == 1):
-			if(msg.m0_x1 > msg.m0_x0):
-				x = (msg.m0_x1 - msg.m0_x0) / frame_width
-				y = (msg.m0_y1 - msg.m0_y0) / frame_height
-			else:
-				x = (msg.m0_x0 - msg.m0_x1) / frame_width
-				y = (msg.m0_y0 - msg.m0_y1) / frame_height
-			if(msg.m0_x0 != msg.m0_x1 and y != 0):
-				steer = (-1) * (x / y)
-				speed = 1.1
-			else:
-				steer = 0
-				speed = 1.0
+        # Time to wait before running
+        self.get_logger().info('Waiting to start for {:s}'.format(str(self.start_delay)))
+        sleep(self.start_delay)
+        self.get_logger().info('Started')
 
-		if(num_vectors == 2):
-			m_x1 = (msg.m0_x1 + msg.m1_x1) / 2
-			steer = (m_x1 - window_center) / frame_width
-			speed = 1.1
+        # Subscribers
+        self.pixy_subscriber = self.create_subscription(
+            PixyVector,
+            self.camera_vector_topic,
+            self.listener_callback,
+            10)
 
-		self.speed_vector.x = float(speed)
-		self.steer_vector.z = float(steer)
+        # Publishers
+        self.cmd_vel_publisher = self.create_publisher(Twist, '/cupcar0/cmd_vel', 10)
 
-		self.cmd_vel.linear = self.speed_vector
-		self.cmd_vel.angular = self.steer_vector
+        self.speed_vector = Vector3()
+        self.steer_vector = Vector3()
+        self.cmd_vel = Twist()
 
-		self.cmd_vel_publisher.publish(self.cmd_vel)
+        # Timer setup
+        # timer_period = 0.5 #seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        # self.i = 0
+
+    def get_num_vectors(self, msg):
+        num_vectors = 0
+        if(not(msg.m0_x0 == 0 and msg.m0_x1 == 0 and msg.m0_y0 == 0 and msg.m0_y1 == 0)):
+            num_vectors = num_vectors + 1
+        if(not(msg.m1_x0 == 0 and msg.m1_x1 == 0 and msg.m1_y0 == 0 and msg.m1_y1 == 0)):
+            num_vectors = num_vectors + 1
+        return num_vectors
+
+    # def timer_callback(self):
+    #     #TODO
+
+    def listener_callback(self, msg):
+        #TODO
+        frame_width = 79
+        frame_height = 52
+        window_center = (frame_width / 2)
+        x = 0
+        y = 0
+        steer = 0
+        speed = 0
+        num_vectors = self.get_num_vectors(msg)
+
+        if(num_vectors == 0):
+            steer = 0
+            speed = 0
+
+        if(num_vectors == 1):
+            if(msg.m0_x1 > msg.m0_x0):
+                x = (msg.m0_x1 - msg.m0_x0) / frame_width
+                y = (msg.m0_y1 - msg.m0_y0) / frame_height
+            else:
+                x = (msg.m0_x0 - msg.m0_x1) / frame_width
+                y = (msg.m0_y0 - msg.m0_y1) / frame_height
+            if(msg.m0_x0 != msg.m0_x1 and y != 0):
+                steer = (-self.angular_velocity) * (x / y)
+                speed = self.linear_velocity
+            else:
+                steer = 0
+                speed = self.linear_velocity*0.9
+
+        if(num_vectors == 2):
+            m_x1 = (msg.m0_x1 + msg.m1_x1) / 2
+            steer = self.angular_velocity*(m_x1 - window_center) / frame_width
+            speed = self.linear_velocity
+
+        self.speed_vector.x = float(speed)
+        self.steer_vector.z = float(steer)
+
+        self.cmd_vel.linear = self.speed_vector
+        self.cmd_vel.angular = self.steer_vector
+
+        self.cmd_vel_publisher.publish(self.cmd_vel)
 
 def main(args=None):
-	rclpy.init(args=args)
+    rclpy.init(args=args)
 
-	line_follow = LineFollow()
+    line_follow = LineFollow()
 
-	rclpy.spin(line_follow)
+    rclpy.spin(line_follow)
 
-	line_follow.destroy_node()
-	rclpy.shutdown()
+    line_follow.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
-	main()
+    main()
